dff3292501e0da8679156391f85d8d969b916c10
Pedro Daia <pedrodaia.c@gmail.com>
2026-03-20 23:41:13 -0300

    frontend client

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-black h-screen w-full">
+      <main>
+        <h1 className="text-white text-center py-4">vovonacci dashboard</h1>
+      </main>
+    </div>
+  );
+}
